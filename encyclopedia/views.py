8c32908c84f588cefc5a5bae925a18eb6c13d436
Gautam Sapre <gautamsapre@gmail.com>
2020-07-16 16:00:47 -0500

    markdown2 implemented

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,10 +2,9 @@ from django import forms
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-
+import markdown2
 
 from . import util
-
 
 
 def index(request):
@@ -19,21 +18,28 @@ def entry(request):
     s = request.POST['q']
     if util.get_entry(s) != None:
         return  render(request, "encyclopedia/entry.html", {
-        "entry": util.get_entry(s)
+        "entry": convert_md(util.get_entry(s))
         })
     else:
-        for entry in util.list_entries():
+        return similar_results(request, s, not_found)
+
+
+def similar_results(request, s,  not_found):
+    for entry in util.list_entries():
             ent = entry.lower()
             if ent.find(s.lower()) != -1:
                 not_found.append(entry)
-        if len(not_found) == 0 :
-            return  render(request, "encyclopedia/entry.html", {
-                "entry": util.get_entry(s)
-            })
-        else:
-            return render(request, "encyclopedia/index.html", {
-                "entries": not_found
-            })
+    if len(not_found) == 0:
+        return  render(request, "encyclopedia/entry.html", {
+            "entry": util.get_entry(s)
+        })
+    else:
+        return render(request, "encyclopedia/index.html", {
+            "entries": not_found
+        })
 
+def convert_md(md_file):
+    md = markdown2.markdown(md_file)
+    return md
  
 
