6fdf408cdadc76832bde745f84610cf2ece4b2b8
Gautam Sapre <gautamsapre@gmail.com>
2020-07-16 11:32:56 -0500

    Search box potentially working

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,22 @@
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
 
 from . import util
+
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+
+def entry(request):
+    s = request.POST['q']
+    return  render(request, "encyclopedia/entry.html", {
+    "entry": util.get_entry(s)
     })
 
