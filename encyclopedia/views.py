1576bf95b127f4c7da50d66f130c0ea411b7e560
Gautam Sapre <gautamsapre@gmail.com>
2020-07-16 14:35:52 -0500

    no result suggestions fixed

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -15,8 +15,26 @@ def index(request):
 
 
 def entry(request):
+    not_found=[]
     s = request.POST['q']
-    return  render(request, "encyclopedia/entry.html", {
-    "entry": util.get_entry(s)
-    })
+    print(s)
+    if util.get_entry(s) != None:
+        return  render(request, "encyclopedia/entry.html", {
+        "entry": util.get_entry(s)
+        })
+    else:
+        for entry in util.list_entries():
+            if entry.find(s) != -1:
+                print(entry)
+                not_found.append(entry)
+        if len(not_found) == 0 :
+            return  render(request, "encyclopedia/entry.html", {
+                "entry": util.list_entries()
+            })
+        else:
+            return render(request, "encyclopedia/index.html", {
+                "entries": not_found
+            })
+
+ 
 
