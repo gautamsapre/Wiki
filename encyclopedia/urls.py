9236acab573b09eaad96eb3ba15e9f68d5303225
Gautam Sapre <gautamsapre@gmail.com>
2020-07-18 16:58:31 -0500

    random page, redirect working

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("entry", views.entry, name = "entry"),
+    path("wiki", views.entry, name = "entry"),
     path("wiki/<s>", views.givenEntry, name = "givenEntry"),
+    path("redir", views.redir, name="redir")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,8 +1,11 @@
 from django import forms
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 import markdown2
+import random 
+from django.shortcuts import redirect
+from django.http import (HttpResponse, HttpResponseNotFound, Http404, 
+HttpResponseRedirect, HttpResponsePermanentRedirect)
 
 from . import util
 
@@ -12,22 +15,20 @@ def index(request):
         "entries": util.list_entries()
     })
 
-
 def entry(request):
     s = request.POST['q']
-    not_found=[]
-    return similar_results(request, s,not_found)
+    return similar_results(request, s)
 
 def givenEntry(request, s):
-    not_found=[]
     if util.get_entry(s) != None:
         return  render(request, "encyclopedia/entry.html", {
         "entry": convert_md(util.get_entry(s))
         })
     else:
-        return similar_results(request, s, not_found)
+        return similar_results(request, s)
 
-def similar_results(request, s,  not_found):
+def similar_results(request, s):
+    not_found=[]
     for entry in util.list_entries():
             ent = entry.lower()
             if ent.find(s.lower()) != -1:
@@ -44,4 +45,6 @@ def similar_results(request, s,  not_found):
 def convert_md(md_file):
     md = markdown2.markdown(md_file)
     return md
- 
+
+def redir(request):
+    return redirect('givenEntry', util.list_entries()[random.randint(0,len(util.list_entries())-1)])
